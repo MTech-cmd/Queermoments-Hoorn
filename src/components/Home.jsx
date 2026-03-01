3f89894d8279cb63dc75c06e4ff7b00550b5228c
Mehdi El Khallouki <mehdi-elkhallouki@pm.me>
2026-03-01 01:25:47 +0100

    Deploying to GH Pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import Hero from "../assets/hero.jpg"
 import Oog from "../assets/oog.png"
+import "./Home.css"
 
 const Home = () => {
   return (
     <div>
-      <div className="flex items-start gap-48 bg-[#dad2da] p-24">
+      <div className="flex items-start gap-72 bg-[#dad2da] p-24">
         
         <div className="flex-1 w-2/3">
           <img 
@@ -13,13 +14,16 @@ const Home = () => {
           />
         </div>
 
-        <div className="flex flex-1 w-24">
-          <h1 className="font-aboreto text-4xl">
-            Toffe queer borrels en meetups in Hoorn!
-          </h1>
-          <img src={Oog} />
+        <div className="flex-1">
+            <h1 className="font-aboreto text-4xl leading-tight">
+                Toffe queer borrels <br /> en meetups <br /> in Hoorn
+            </h1>
         </div>
-
+            <img 
+            src={Oog} 
+            className="absolute mt-96 mr-24 oog"
+            alt=""
+            />
       </div>
     </div>
   )
